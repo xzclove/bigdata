b19ea4c26f7a28d75fff451de76716d63968c676
925654140 <925654140@qq.com>
2016-05-26 21:04:17 +0800

    hdfs-test

---
+++ hdfs-test/src/main/java/com/ibeifeng/hdfs/test/hdfs/App.java
@@ -0,0 +1,13 @@
+package com.ibeifeng.hdfs.test.hdfs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
