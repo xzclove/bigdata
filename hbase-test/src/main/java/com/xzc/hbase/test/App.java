b97a37745dfa9cde346bf7bd14b0d2d70ef803e3
feelingxu <feelingxu@tcl.com>
2016-06-05 23:30:41 +0800

    add hdfs

---
+++ hbase-test/src/main/java/com/xzc/hbase/test/App.java
@@ -0,0 +1,13 @@
+package com.xzc.hbase.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
